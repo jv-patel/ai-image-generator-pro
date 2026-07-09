8d664954b87a9dd9379820538d96ff1950f76d27
Jeel Patel <159604570+jv-patel@users.noreply.github.com>
2026-07-09 21:35:21 +0530

    Rewrite utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
 from io import BytesIO
 from zipfile import ZipFile
-from PIL import Image
 
 
-def image_to_bytes(image: Image.Image):
+def image_to_bytes(image):
     """
     Convert PIL Image to PNG bytes.
     """
@@ -15,19 +14,20 @@ def image_to_bytes(image: Image.Image):
 
 def create_zip(images):
     """
-    Create ZIP file from a list of PIL Images.
-    Returns ZIP bytes.
+    Create a ZIP file containing all generated images.
     """
     zip_buffer = BytesIO()
 
     with ZipFile(zip_buffer, "w") as zip_file:
+
         for index, image in enumerate(images, start=1):
-            img_buffer = BytesIO()
-            image.save(img_buffer, format="PNG")
+
+            image_buffer = BytesIO()
+            image.save(image_buffer, format="PNG")
 
             zip_file.writestr(
                 f"ai_image_{index}.png",
-                img_buffer.getvalue()
+                image_buffer.getvalue(),
             )
 
     zip_buffer.seek(0)
